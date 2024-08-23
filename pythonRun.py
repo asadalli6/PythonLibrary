f1bb9ccbc6080140ed62719424608ee78150ad4e
Asad Alli <46456482+asadalli6@users.noreply.github.com>
2024-08-22 21:52:23 -0400

    LinkedList

--- pythonRun.py
+++ pythonRun.py
@@ -1,29 +1,148 @@
+import _tkinter as TK
 
-def CalculateDebt():
-    print("Credit Card Calculator - Asad Alli 2024")
-    totalChecking = input("Checkings total amount: ")
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-    TDCardMax = 6000.00
-    TDCardCreditLeft = input("Input total available TD credit: ")
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-    totalTDOwed = TDCardMax - float(TDCardCreditLeft)
-    totalTDOwed = round(totalTDOwed, 2)
+    def insertAtBegin(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            new_node.next = self.head
+            self.head = new_node
 
-    AmexCardMax = 10000.00
-    AmexCardCreditLeft = input("Input total available Amex credit: ")
+    def insertAtIndex(self, data, index):
+        if (index == 0):
+            self.insertAtBegin(data)
+            
+        position = 0
+        current_node = self.head
+        while (current_node != None and position+1 != index):
+            position = position+1
+            current_node = current_node.next
 
-    totalAmexOwed = AmexCardMax - float(AmexCardCreditLeft)
-    totalAmexOwed = round(totalAmexOwed, 2)
+        if current_node != None:
+            new_node = Node(data)
+            new_node.next = current_node.next
+            current_node.next = new_node
+        else:
+            print("Index not present")
 
-    print("\n\nTotal TD Debt: " + str(totalTDOwed))
-    print("Total AMEX owed: " + str(totalAmexOwed))
 
-    totalOwed = float(totalTDOwed) + float(totalAmexOwed)
-    totalOwed = round(totalOwed, 2)
-    print("Total credit due: " + str(totalOwed))
+    def insertAtEnd(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
 
-    newCheckingAmount = float(totalChecking) - totalOwed
-    newCheckingAmount = round(newCheckingAmount, 2)
-    print("Total checking amount after credit debt: " + str(newCheckingAmount))
+        current_node = self.head
+        while(current_node.next):
+            current_node = current_node.next
 
-CalculateDebt()
+        current_node.next = new_node
+
+    def updateNode(self, val, index):
+        current_node = self.head
+        position = 0
+        if position == index:
+            current_node.data = val
+        else:
+            while(current_node != None and position != index):
+                position = position+1
+                current_node = current_node.next
+
+            if current_node != None:
+                current_node.data = val
+            else:
+                print("Index not present")
+
+    def remove_first_node(self):
+        if(self.head == None):
+            return
+
+        self.head = self.head.next
+
+    def remove_last_node(self):
+
+        if self.head is None:
+            return
+
+        current_node = self.head
+        while(current_node != None and current_node.next.next != None):
+            current_node = current_node.next
+
+        current_node.next = None
+
+    def remove_at_index(self, index):
+        if self.head == None:
+            return
+
+        current_node = self.head
+        position = 0
+        if position == index:
+            self.remove_first_node()
+        else:
+            while(current_node != None and position+1 != index):
+                position = position+1
+                current_node = current_node.next
+
+            if current_node != None:
+                current_node.next = current_node.next.next
+            else:
+                print("Index not present")
+
+    def remove_node(self, data):
+        current_node = self.head
+
+        if current_node.data == data:
+            self.remove_first_node()
+            return
+
+        while(current_node != None and current_node.next.data != data):
+            current_node = current_node.next
+
+        if current_node == None:
+            return
+        else:
+            current_node.next = current_node.next.next
+
+    def sizeOfLL(self):
+        size = 0
+        if(self.head):
+            current_node = self.head
+            while(current_node):
+                size = size+1
+                current_node = current_node.next
+            return size
+        else:
+            return 0
+
+    def printLL(self):
+        current_node = self.head
+        while(current_node):
+            print(current_node.data)
+            current_node = current_node.next
+
+#Python Main Loop
+llist = LinkedList()
+
+llist.insertAtBegin('1')
+llist.insertAtBegin('2')
+llist.insertAtBegin('3')
+llist.insertAtBegin('4')
+llist.insertAtBegin('5')
+llist.insertAtBegin('6')
+llist.insertAtBegin('7')
+llist.insertAtBegin('8')
+
+print("Display Linked List: ")
+llist.printLL()
+
+print("\nNodes in linked list: " + str(llist.sizeOfLL()))
